0296a50380427a7fe6e948eed71705cb76fcffda
androozhang <zandrew129@gmail.com>
2024-01-15 20:41:31 -0500

    Connected firebase to Login.tsx

---
+++ FirebaseConfig.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5tu0yoRtnRM-ftMKH0VgOe_TrmOnqxLE",
+  authDomain: "gymtracker-b8086.firebaseapp.com",
+  projectId: "gymtracker-b8086",
+  storageBucket: "gymtracker-b8086.appspot.com",
+  messagingSenderId: "583572974340",
+  appId: "1:583572974340:web:b2d3be18dca7aa7ab3be51",
+  measurementId: "G-PF6N6PT4TL"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_ANALYTICS = getAnalytics(FIREBASE_APP);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
---
+++ app/screens/Login.tsx
@@ -0,0 +1,84 @@
+import { View, Text, TextInput, StyleSheet, ActivityIndicator, Button, KeyboardAvoidingView } from 'react-native'
+import React from 'react'
+import { FIREBASE_AUTH } from '../../FirebaseConfig';
+import { FIRESTORE_DB } from '../../FirebaseConfig';  
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, collection, doc, setDoc, addDoc } from 'firebase/firestore';
+
+const Login = () => {
+  const [name, setName] = React.useState(''); 
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const auth = FIREBASE_AUTH;
+  const myCollection = collection(FIRESTORE_DB, 'myCollection');
+  
+  const signIn = async() => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error: any) {
+      alert('Sign in failed: ' + error.message);
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const signUp = async() => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(auth, email, password);
+      const userCollection = collection(FIRESTORE_DB, 'userCollection');
+      const userData = {
+        userId: response.user.uid,
+        name: name,
+        email: email,
+      };
+      const newDocRef = await addDoc(userCollection, userData);
+      console.log('New document added with ID:', newDocRef.id);
+      alert("Check your emails!")
+    } catch (error: any) {
+      alert('Sign in failed: ' + error.message);
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <View style={style.container}>
+      <KeyboardAvoidingView behavior='padding'>
+        <TextInput value={name} style={style.input} placeholder='Full Name' onChangeText={(text) => setName(text)}></TextInput>
+        <TextInput value={email} style={style.input} placeholder='Email' autoCapitalize='none' onChangeText={(text) => setEmail(text)}></TextInput>
+        <TextInput secureTextEntry={true} value={password} style={style.input} placeholder='Password' autoCapitalize='none' onChangeText={(text) => setPassword(text)}></TextInput>
+    { loading ? (<ActivityIndicator size="large" color="#0000ff" /> 
+    ) : ( 
+        <> 
+          <Button title="Login" onPress={signIn} />
+          <Button title="Create Account" onPress={signUp} />
+        </>
+    )}
+        </KeyboardAvoidingView>
+    </View>
+  )
+}
+
+export default Login
+
+const style = StyleSheet.create({
+    container: {
+        marginHorizontal: 20,
+        flex: 1,
+        justifyContent: 'center'
+    },
+    input: {
+        marginVertical: 4, 
+        height: 50, 
+        borderWidth: 1, 
+        borderRadius: 4, 
+        padding: 10, 
+        backgroundColor: '#fff'
+    }
+});
